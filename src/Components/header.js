98c81b48130a4bdcc22b80d3197ff121848fefe3
HichamOujari <hichamoujari99@gmail.com>
2020-11-29 01:07:22 +0100

    change 1

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,13 +1,10 @@
 import React,{Component} from "react"
-import {Link,BrowserRouter} from "react-router-dom"
 
 class Header extends Component {
     render(){
         return (
             <div>
-                <BrowserRouter>
-                    <Link to="/" ><h1 className="supertitle">Random<span className="soustitle">Chat</span></h1></Link>
-                </BrowserRouter>
+                    <a href="/" ><h1 className="supertitle">Random<span className="soustitle">Chat</span></h1></a>
             </div>
         )
     }

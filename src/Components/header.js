f86630a89190841507d62acb1074a657b41020f5
HichamOujari <hichamoujari99@gmail.com>
2020-11-29 01:00:59 +0100

    change 1

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,10 +1,13 @@
 import React,{Component} from "react"
+import {Link,BrowserRouter} from "react-router-dom"
 
 class Header extends Component {
     render(){
         return (
             <div>
-                <h1 className="supertitle">Random<span className="soustitle">Chat</span></h1>
+                <BrowserRouter>
+                    <Link to="/" ><h1 className="supertitle">Random<span className="soustitle">Chat</span></h1></Link>
+                </BrowserRouter>
             </div>
         )
     }
--- src/Components/join.js
+++ src/Components/join.js
@@ -3,12 +3,16 @@ import joinlogo from "../Assets/logojoin.png"
 
 class Join extends Component {
     render(){
+        const auth = (e) => {
+            e.preventDefault();
+            window.location.href="/chat/"+document.getElementById("username").value;
+        }
         return (
             <div>
-                <form className="formjoin">
+                <form onSubmit={auth} className="formjoin">
                     <img className="joinlogo" alt="logo" src={joinlogo} />
-                    <input className="joininput" type="text" name="username" autoComplete="off" placeholder="Enter your username ..."/>
-                    <input className="joininput" type="text" name="Age" autoComplete="off" placeholder="Enter your Age ..."/>
+                    <input className="joininput" type="text" required id="username" name="username" autoComplete="off" placeholder="Enter your username ..."/>
+                    <input className="joininput" type="number" required name="Age" autoComplete="off" placeholder="Enter your Age ..."/>
                     <input className="joinsubmit" value="Connect" type="submit" />
                 </form>
             </div>

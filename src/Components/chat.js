83c101cd7ac9351028cda777b4750f463a7cf3e5
HichamOujari <hichamoujari99@gmail.com>
2020-12-12 01:43:02 +0100

    add video call

--- src/Components/chat.js
+++ src/Components/chat.js
@@ -1,8 +1,9 @@
 import React,{Component,Fragment} from "react"
 import loading from "../Assets/loading.gif"
 import io from "socket.io-client"
+import Peer from 'peerjs'
 
-const socket = io.connect('https://randomchat-server.herokuapp.com:4000')
+const socket = io.connect('https://randomchat-server.herokuapp.com')
 
 function scroll(old){
     setTimeout(()=>{
@@ -11,11 +12,18 @@ function scroll(old){
         if(old===-1 || old!==sc.scrollTop){
             scroll(sc.scrollTop)
         }
-    },5);
+    },30);
 }
 function perdue() {
     document.querySelector(".couser").removeAttribute("hidden");
     document.querySelector("#videoElement2").setAttribute("hidden","");
+    document.querySelector("#msg").disabled="true";
+}
+
+function affiche() {
+    document.querySelector("#videoElement2").removeAttribute("hidden");
+    document.querySelector(".couser").setAttribute("hidden","");
+    document.querySelector("#msg").removeAttribute("disabled");
 }
 
 class Chat extends Component {
@@ -31,62 +39,100 @@ class Chat extends Component {
         if(ele.value.length!==0){
             var time = new Date();
             time=time.toTimeString().split(':');
-            time=time[0]+":"+time[1]
-            socket.emit('new_message',{to:this.state.couser.id,id:this.state.user.id,msg:ele.value,time:time})
+            time=time[0]+":"+time[1];
+            socket.emit("new_msg",{to:this.state.couser.id,msg:ele.value,time:time});
+            this.setState({
+                msg:[...this.state.msg,{to:this.state.couser.id,msg:ele.value,time:time}],
+            })
             ele.value="";
-            scroll(-1);
         }
     }
     skip(){
-        perdue()
-        socket.emit("skip",{id:this.state.user.id,coid:this.state.couser.id})
+        perdue();
+        this.setState({
+            couser:{id:" ",name:" "}
+        })
+        socket.emit("skip",{name:this.state.user.name,peerid:this.state.user.peerid});
+    }
+    next(){
+        socket.emit("skip",this.state.user.name);
+        document.querySelector("#skiped").style.display="none";
     }
     stream = (dst,nbr) => {
-        var video = document.querySelector(dst)
+        var video = document.querySelector(dst);
         if (navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({ video: true })
                 .then(function (stream) {
                     if(video!=null){
                         document.querySelectorAll(".loading")[nbr].setAttribute("hidden","");
                         video.removeAttribute('hidden');
-                        video.srcObject=stream
+                        video.srcObject=stream;
                     }
                 })
         }
     }
+
     componentDidMount(){
-        socket.on('message',data=>{
+        let peer = new Peer();
+        let peerid=null
+        peer.on('open',id=>{
+            peerid=id;
+            socket.emit("mydata",{name:this.props.match.params.user,peerid:id});
+        })
+        socket.on("mydata",data=>{
             this.setState({
-                msg:[...this.state.msg,{id:data.id,msg:data.msg,time:data.time}]})
+                user:{id:data,name:this.props.match.params.user,peerid:peerid}
+            })
+            this.stream("#videoElement",0);
+        })
+        socket.on("new_msg",data=>{
+            this.setState({
+                msg:[...this.state.msg,data],
+            })
         })
         socket.on('count',data=>{
             this.setState({
                 online: data,
             })
         })
-        socket.on('id',data=>{
-            this.stream("#videoElement",0)
+        socket.on("couser",data=>{
             this.setState({
-                user:{id:data,name:this.props.match.params.user},
-            })    
-        })
-        socket.on('coId',data=>{
-            this.stream("#videoElement2",1)
-            this.setState({
-                couser:{id:data.id,name:data.name},
+                couser:{id:data.id,name:data.name,peerid:data.peerid}
             })
+            navigator.mediaDevices.getUserMedia({video: true, audio: true})
+                .then(stream=>{
+                    peer.call(data.peerid,stream)
+                })
+                .catch(err=>{console.log('Error in first one : '+err)})
+            affiche();
         })
-        socket.on('exited',data=>{
-            perdue()
+        peer.on('call',call=>{
+            navigator.mediaDevices.getUserMedia({video: true, audio: true})
+                .then(stream=>{
+                    call.answer(stream);
+                    call.on('stream', function(remoteStream) {
+                        document.querySelector("#videoElement2").srcObject=remoteStream;
+                    });
+                })
+            }, function(err) {
+            console.log('Failed to get local stream' ,err);
+        })
+        socket.on("camera",data=>{
+            document.querySelector("#videoElement2").srcObject=window.URL.createObjectURL(data)
+        })
+        socket.on("exited",()=>{
+            perdue();
             this.setState({
                 couser:{id:" ",name:" "},
             })
+            document.querySelector("#skiped").style.display="block";
         })
     }
+
     render(){
         var key=0;
         const list = this.state.msg.map((i)=>{
-            const type=(i.id===this.state.user.id)?"this":"other"
+            const type=(i.to!==this.state.user.id)?"this":"other"
             scroll(-1);
             return(<Fragment key={key++}>
                         <p className={"content "+type}>{i.msg}</p><p className="heure">{i.time}</p><br/>
@@ -118,9 +164,13 @@ class Chat extends Component {
                 <form  onSubmitCapture={this.send} className="mailing">
                     <div id="msgcontent" className="msgcontent">
                         {list}
-                        
+                        <div className="skiped" id="skiped">
+                            <hr/>
+                            <p>You have been skipped : </p>
+                            <p className="btnnew" onClick={this.next.bind(this)}>New</p>
+                        </div>
                     </div>
-                    <input type="text" id="msg" autoComplete="off" placeholder="Enter your message ..." name="msg" className="msg" autoFocus={true} />
+                    <input type="text" id="msg" autoComplete="off" disabled placeholder="Enter your message ..." name="msg" className="msg" autoFocus={true} />
                     <button className="send" >Send</button>
                     <button className="skip" onClick={this.skip.bind(this)}>Skip</button>
                 </form>
